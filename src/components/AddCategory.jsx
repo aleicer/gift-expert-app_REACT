7eca6733376aa1dd80621d0500577380de045db7
Aleicer <aleicer.vesga@gmail.com>
2023-01-02 08:47:36 -0500

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export const AddCategory = ({ onNewCategory }) => {
+
+  const [inputValue, setInputValue] = useState('')
+
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (inputValue.trim().length <= 1) return
+
+    // addCategories( categories => [inputValue, ...categories])
+    onNewCategory( inputValue.trim() )
+    setInputValue('')
+  }
+
+  return (
+    <form onSubmit= { onSubmit }>
+        <input
+          type="text"
+          placeholder="Buscar Gift"
+          value={ inputValue }
+          onChange= { onInputChange }
+        />
+    </form>
+    
+  )
+}
